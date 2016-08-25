651fee9a5889b47515b898a43fb679f30c739b97
Andreas de Pretis <ad@25th-floor.com>
2016-08-25 17:43:12 +0200

    Initial commit

---
+++ 25th-floor.mk
@@ -0,0 +1,47 @@
+BLUE      := $(shell tput -Txterm setaf 4)
+GREEN     := $(shell tput -Txterm setaf 2)
+TURQUOISE := $(shell tput -Txterm setaf 6)
+WHITE     := $(shell tput -Txterm setaf 7)
+YELLOW    := $(shell tput -Txterm setaf 3)
+RESET     := $(shell tput -Txterm sgr0)
+
+SMUL      := $(shell tput smul)
+RMUL      := $(shell tput smul)
+
+# Add the following 'help' target to your Makefile
+# And add help text after each target name starting with '\#\#'
+# A category can be added with @category
+HELP_FUN = \
+	%help; \
+	while(<>) { push @{$$help{$$2 // 'options'}}, [$$1, $$3] if /^([a-zA-Z\-]+)\s*:.*\#\#(?:@([a-zA-Z\-]+))?\s(.*)$$/ }; \
+	for (sort keys %help) { \
+	printf("${WHITE}%24s:${RESET}\n\n", $$_); \
+	for (@{$$help{$$_}}) { \
+	printf("${YELLOW}%25s${RESET}${GREEN}  %s${RESET}\n", $$_->[0], $$_->[1]); \
+	} \
+	print "\n"; } 
+
+# make
+.DEFAULT_GOAL := help
+
+# Variable wrapper
+define defw
+	custom_vars += $(1)
+	$(1) := $(2)
+	export $(1)
+endef
+
+.PHONY: help
+help:: ##@Other Show this help.
+	@echo ""
+	@printf "%30s " "${BLUE}VARIABLES"
+	@echo "${RESET}"
+	@echo ""
+	@printf "${BLUE}%25s${RESET}${TURQUOISE}  ${SMUL}%s${RESET}\n" $(foreach v, $(custom_vars), $v $($(v)))
+	@echo ""
+	@echo ""
+	@echo ""
+	@printf "%30s " "${YELLOW}TARGETS"
+	@echo "${RESET}"
+	@echo ""
+	@perl -e '$(HELP_FUN)' $(MAKEFILE_LIST)

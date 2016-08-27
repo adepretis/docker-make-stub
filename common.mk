a81e8ead5ea105ca0202caa00020ddf37724ceb9
Andreas de Pretis <ad@25th-floor.com>
2016-08-27 22:52:52 +0200

    Color-code targets & description for Makefile (assumption: implemented) and others (assumption: not implemented)

--- common.mk
+++ common.mk
@@ -3,26 +3,33 @@ GREEN     := $(shell tput -Txterm setaf 2)
 TURQUOISE := $(shell tput -Txterm setaf 6)
 WHITE     := $(shell tput -Txterm setaf 7)
 YELLOW    := $(shell tput -Txterm setaf 3)
+GREY      := $(shell tput -Txterm setaf 1)
 RESET     := $(shell tput -Txterm sgr0)
 
 SMUL      := $(shell tput smul)
-RMUL      := $(shell tput smul)
+RMUL      := $(shell tput rmul)
 
 # Add the following 'help' target to your Makefile
 # And add help text after each target name starting with '\#\#'
 # A category can be added with @category
 HELP_FUN = \
 	%help; \
+	use Data::Dumper; \
 	while(<>) { \
 		if (/^([a-zA-Z\-]+)\s*:.*\#\#(?:@([a-zA-Z\-]+))?\s(.*)$$/) { \
 			$$c = $$2; $$t = $$1; $$d = $$3; \
-			push @{$$help{$$c}}, [$$t, $$d] unless grep { grep { grep /^$$t$$/, $$_->[0] } @{$$help{$$_}} } keys %help; \
+			push @{$$help{$$c}}, [$$t, $$d, $$ARGV] unless grep { grep { grep /^$$t$$/, $$_->[0] } @{$$help{$$_}} } keys %help; \
 		} \
 	}; \
 	for (sort keys %help) { \
 		printf("${WHITE}%24s:${RESET}\n\n", $$_); \
 		for (@{$$help{$$_}}) { \
-			printf("${YELLOW}%25s${RESET}${GREEN}  %s${RESET}\n", $$_->[0], $$_->[1]); \
+			printf("%s%25s${RESET}%s  %s${RESET}\n", \
+				( $$_->[2] eq "Makefile" || $$_->[0] eq "help" ? "${YELLOW}" : "${GREY}"), \
+				$$_->[0], \
+				( $$_->[2] eq "Makefile" || $$_->[0] eq "help" ? "${GREEN}" : "${GREY}"), \
+				$$_->[1] \
+			); \
 		} \
 		print "\n"; \
 	} 
